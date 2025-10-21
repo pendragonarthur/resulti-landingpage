85865804a9a48a4257bbcad84154d189c889fa0a
Arthur Quaresma <devarthurquarxma@outlook.com>
2025-10-21 10:32:13 -0300

    deleted vercel.json

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,13 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "1337",
+        protocol: "https",
+        hostname: "bright-confidence-21b928e339.strapiapp.com",
         pathname: "/uploads/**",
       },
     ],
