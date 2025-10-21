ce1939857b0c9e0da4113af828e456f9ccd3de0c
Arthur Quaresma <devarthurquarxma@outlook.com>
2025-10-21 11:33:00 -0300

    fix cors middleware

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -1,12 +1,24 @@
-export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+module.exports = [
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: [
+        "http://localhost:3000",
+        "https://bright-confidence-21b928e339.strapiapp.com/admin",
+      ],
+      headers: ["Content-Type", "Authorization"],
+    },
+  },
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
